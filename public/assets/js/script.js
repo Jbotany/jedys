68fd3352ef23c7881f3bb31b468d8f0f06d55827
laheravilla <laheravilla@gmail.com>
2019-04-19 01:48:39 +0200

    ajout button up

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,3 +4,21 @@
 //         $(this).css('box-shadow', '10px 10px 20px black');
 //     });
 // });
+
+$(document).ready(function() {
+
+    $('.buttonUp').click(function() {
+        $('body, html').animate({
+            scrollTop: 0,
+        }, 300);
+
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100 ){
+            $('.buttonUp').fadeIn('1000');
+        } else {
+            $('.buttonUp').fadeOut('1000');
+        }
+    });
+});
