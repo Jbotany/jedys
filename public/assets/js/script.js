3036d62d4ef634548c6e8a02fc1846b20dfc1b09
laheravilla <laheravilla@gmail.com>
2019-04-19 08:28:26 +0200

    counter

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -15,3 +15,37 @@ $(document).ready(function() {
         }
     });
 });
+
+$(document).ready(function () {
+
+    let counter = 20;
+    $('.counter').text(counter);
+
+    $('.egg-007').click(function () {
+        $(this).hide();
+        counter--;
+        $('.counter').text(counter);
+    });
+    $('.egg-014').click(function () {
+        $(this).hide();
+        counter--;
+        $('.counter').text(counter);
+    });
+    $('.egg-015').click(function () {
+        $(this).hide();
+        counter--;
+        $('.counter').text(counter);
+    });
+    $('.egg-017').click(function () {
+        $(this).hide();
+        counter--;
+        $('.counter').text(counter);
+    });
+    $('.egg-018').click(function () {
+        $(this).hide();
+        counter--;
+        $('.counter').text(counter);
+    });
+
+
+});
